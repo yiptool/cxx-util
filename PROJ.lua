7dc10f48c25ecd21eaa23e4f131d264d53ceafb4
Nikolay Zapolnov <zapolnov@gmail.com>
2014-02-06 00:04:49 +0400

    Publish some header files.

--- PROJ.lua
+++ PROJ.lua
@@ -1,6 +1,11 @@
 
 SUBPROJECT("cxx-util")
 
+DEFINES {
+	"ASSERT_H=\"" .. CURRENT_SOURCE_DIR .. "/assert.h\"",
+	"LOGGER_H=\"" .. CURRENT_SOURCE_DIR .. "/logger.h\"",
+}
+
 SOURCE_FILES {
 	"assert.cpp",
 	"assert.h",
