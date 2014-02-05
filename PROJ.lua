6b28e0e9b87f7a05dfe12fd9e6b770c630881e2e
Nikolay Zapolnov <zapolnov@gmail.com>
2014-02-06 01:47:20 +0400

    Small fix.

--- PROJ.lua
+++ PROJ.lua
@@ -4,6 +4,8 @@ SUBPROJECT("cxx-util")
 DEFINES {
 	"ASSERT_H=\"" .. CURRENT_SOURCE_DIR .. "/assert.h\"",
 	"LOGGER_H=\"" .. CURRENT_SOURCE_DIR .. "/logger.h\"",
+	"MACROS_H=\"" .. CURRENT_SOURCE_DIR .. "/macros.h\"",
+	"FMT_H=\"" .. CURRENT_SOURCE_DIR .. "/fmt.h\"",
 }
 
 SOURCE_FILES {
