5e98de1cf3cf2a62bf2c8c93f1b5b1aa88720ce9
Nikolay Zapolnov <zapolnov@gmail.com>
2014-03-01 12:15:06 +0400

    Better comparison operators for StrongPtr.

--- strong_ptr.h
+++ strong_ptr.h
@@ -57,16 +57,44 @@ public:
 	inline bool operator==(const TYPE * p) const { return m_Ptr == p; }
 	inline bool operator==(const StrongPtr<TYPE> & s) const { return m_Ptr == s.m_Ptr; }
 	inline bool operator==(const WeakPtr<TYPE> & s) const { return m_Ptr == s.get_(); }
-	template <class T> inline bool operator==(const T * p) const { return m_Ptr == p; }
-	template <class T> inline bool operator==(const StrongPtr<T> & s) const { return m_Ptr == s.m_Ptr; }
-	template <class T> inline bool operator==(const WeakPtr<T> & s) const { return m_Ptr == s.get_(); }
+	template <class T> inline bool operator==(const T * p) const { return m_Ptr == static_cast<TYPE *>(p); }
+	template <class T> inline bool operator==(const StrongPtr<T> & s) const { return m_Ptr == static_cast<TYPE *>(s.m_Ptr); }
+	template <class T> inline bool operator==(const WeakPtr<T> & s) const { return m_Ptr == static_cast<TYPE *>(s.get_()); }
 
 	inline bool operator!=(const TYPE * p) const { return m_Ptr != p; }
 	inline bool operator!=(const StrongPtr<TYPE> & s) const { return m_Ptr != s.m_Ptr; }
 	inline bool operator!=(const WeakPtr<TYPE> & s) const { return m_Ptr != s.get_(); }
-	template <class T> inline bool operator!=(const T * p) const { return m_Ptr != p; }
-	template <class T> inline bool operator!=(const StrongPtr<T> & s) const { return m_Ptr != s.m_Ptr; }
-	template <class T> inline bool operator!=(const WeakPtr<T> & s) const { return m_Ptr != s.get_(); }
+	template <class T> inline bool operator!=(const T * p) const { return m_Ptr != static_cast<TYPE *>(p); }
+	template <class T> inline bool operator!=(const StrongPtr<T> & s) const { return m_Ptr != static_cast<TYPE *>(s.m_Ptr); }
+	template <class T> inline bool operator!=(const WeakPtr<T> & s) const { return m_Ptr != static_cast<TYPE *>(s.get_()); }
+
+	inline bool operator<(const TYPE * p) const { return m_Ptr < p; }
+	inline bool operator<(const StrongPtr<TYPE> & s) const { return m_Ptr < s.m_Ptr; }
+	inline bool operator<(const WeakPtr<TYPE> & s) const { return m_Ptr < s.get_(); }
+	template <class T> inline bool operator<(const T * p) const { return m_Ptr < static_cast<TYPE *>(p); }
+	template <class T> inline bool operator<(const StrongPtr<T> & s) const { return m_Ptr < static_cast<TYPE *>(s.m_Ptr); }
+	template <class T> inline bool operator<(const WeakPtr<T> & s) const { return m_Ptr < static_cast<TYPE *>(s.get_()); }
+
+	inline bool operator<=(const TYPE * p) const { return m_Ptr <= p; }
+	inline bool operator<=(const StrongPtr<TYPE> & s) const { return m_Ptr <= s.m_Ptr; }
+	inline bool operator<=(const WeakPtr<TYPE> & s) const { return m_Ptr <= s.get_(); }
+	template <class T> inline bool operator<=(const T * p) const { return m_Ptr <= static_cast<TYPE *>(p); }
+	template <class T> inline bool operator<=(const StrongPtr<T> & s) const { return m_Ptr <= static_cast<TYPE *>(s.m_Ptr); }
+	template <class T> inline bool operator<=(const WeakPtr<T> & s) const { return m_Ptr <= static_cast<TYPE *>(s.get_()); }
+
+	inline bool operator>(const TYPE * p) const { return m_Ptr > p; }
+	inline bool operator>(const StrongPtr<TYPE> & s) const { return m_Ptr > s.m_Ptr; }
+	inline bool operator>(const WeakPtr<TYPE> & s) const { return m_Ptr > s.get_(); }
+	template <class T> inline bool operator>(const T * p) const { return m_Ptr > static_cast<TYPE *>(p); }
+	template <class T> inline bool operator>(const StrongPtr<T> & s) const { return m_Ptr > static_cast<TYPE *>(s.m_Ptr); }
+	template <class T> inline bool operator>(const WeakPtr<T> & s) const { return m_Ptr > static_cast<TYPE *>(s.get_()); }
+
+	inline bool operator>=(const TYPE * p) const { return m_Ptr >= p; }
+	inline bool operator>=(const StrongPtr<TYPE> & s) const { return m_Ptr >= s.m_Ptr; }
+	inline bool operator>=(const WeakPtr<TYPE> & s) const { return m_Ptr >= s.get_(); }
+	template <class T> inline bool operator>=(const T * p) const { return m_Ptr >= static_cast<TYPE *>(p); }
+	template <class T> inline bool operator>=(const StrongPtr<T> & s) const { return m_Ptr >= static_cast<TYPE *>(s.m_Ptr); }
+	template <class T> inline bool operator>=(const WeakPtr<T> & s) const { return m_Ptr >= static_cast<TYPE *>(s.get_()); }
 
 private:
 	TYPE * m_Ptr;
