49117692425c2bb86882a9dbc4f0e55e14d343fd
Nikolay Zapolnov <zapolnov@gmail.com>
2014-04-22 01:32:36 +0400

    Added the explode() function.

---
+++ cxx-util/explode.h
@@ -0,0 +1,31 @@
+/* vim: set ai noet ts=4 sw=4 tw=115: */
+//
+// Copyright (c) 2014 Nikolay Zapolnov (zapolnov@gmail.com).
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#ifndef __4ddb1c338e887cdf84e79132561d7ec2__
+#define __4ddb1c338e887cdf84e79132561d7ec2__
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> explode(const std::string & str, char separator);
+
+#endif
