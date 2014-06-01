8846bd3a2103cd991999c2b8806e0a59fdb33c25
Nikolay Zapolnov <zapolnov@gmail.com>
2014-06-01 15:16:10 +0400

    Added missing license info.

--- cxx-util/type_info.h
+++ cxx-util/type_info.h
@@ -1,3 +1,27 @@
+/* vim: set ai noet ts=4 sw=4 tw=115: */
+//
+// Copyright (c) 2014 Nikolay Zapolnov (zapolnov@gmail.com).
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#ifndef __357ac8eb2d1dfd0326f7a55900ab78df__
+#define __357ac8eb2d1dfd0326f7a55900ab78df__
 
 #include "macros.h"
 #include <typeinfo>
@@ -30,3 +54,5 @@ template <class TYPE> inline FCONST TypeInfo typeInfo(TYPE)
 {
 	return typeInfo<TYPE>();
 }
+
+#endif
